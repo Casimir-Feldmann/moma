5292f6542579d91785a0ed65ff94877c0ef94832
Casimir-Feldmann <cfeldmann@student.ethz.ch>
2023-12-15 11:20:04 +0100

    new python library

--- docker/scripts/install_python_packages.sh
+++ docker/scripts/install_python_packages.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y
 sudo apt-get install python3-tk -y
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
-pip3 install pyransac3d imageio einops scikit-learn
+pip3 install pyransac3d imageio einops scikit-learn scikit-image
 
 # Clear cache to keep layer size down
 rm -rf /var/lib/apt/lists/*
